e005eae39bc942631bb414fcb42597dccb8d745e
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-06-07 14:24:31 +0700

    add videos, add images, add header

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
-# React + Vite
+<a href="https://www.youtube.com/watch?v=vXHDfrFMoog&list=PL67b5wgxuUtAzzyFFMZ8W3w_XV4jJfH-8" target="_blank">
+  <img src="./thumbnail.png" alt="Thumbnail"/>
+</a>
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
+---
+## Introduction: 
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+#### 🎮 How to Build a 3D Gaming Website with React, Tailwind CSS, and Spline (Beginner Friendly) 🚀
+#### Step-by-step tutorial with clean animations & modern web design tools!
 
-## Expanding the ESLint configuration
+#### Are you ready to take your web development skills to the next level? In this beginner-friendly tutorial, you’ll learn how to build a fully interactive 3D gaming website using React.js, Tailwind CSS, and Spline. We’ll also add smooth scroll animations and stunning Framer Motion transitions to bring your project to life.
 
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+#### This is not just a coding tutorial — it’s a full guide to creating a visually impressive and responsive website with modern UI/UX design principles. Whether you’re just starting with React or looking to spice up your frontend skills with 3D visuals and animations, this video is for you.
+
+#### 🧩 What You’ll Learn
+
+#### ✅ How to set up a React project with Tailwind CSS
+#### ✅ Integrate 3D Spline models into your site
+#### ✅ Add smooth scroll-based animations
+#### ✅ Use Framer Motion for interactive transitions
+#### ✅ Adding interactive animations and transitions🎨
+
+#### ✅Create a clean, futuristic gaming landing page
+
+#### This tutorial is perfect for beginners & advanced developers who want to build eye-catching, interactive websites.🎯
+
+
+---
+## Watch the full tutorial on YouTub
+<a href="https://www.youtube.com/watch?v=vXHDfrFMoog&list=PL67b5wgxuUtAzzyFFMZ8W3w_XV4jJfH-8">
+  <img src="./youtube.png" alt="youtube"/>
+</a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,19 @@
-import 'boxicons/css/boxicons.min.css'
+import { FaRegUserCircle } from "react-icons/fa";
+import { BsShop } from "react-icons/bs";
+import { IoDiamondOutline } from "react-icons/io5";
+import { MdKeyboardDoubleArrowUp } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+import { IoMdMenu } from "react-icons/io";
+import { useState } from "react";
 
 export default function Header() {
+
+    const [menu, setMenu] = useState(false)
+
+    const handleMenu = () => {
+        setMenu(!menu)
+    }
+
     return (
         <header className="py-1 px-7 flex justify-between items-center fixed top-0 z-50 w-full border-b-[0.3px] border-[#babaff]">
             <div className="flex lg:gap-14 gap-4 items-center">
@@ -15,11 +28,53 @@ export default function Header() {
                 </div>
             </div>
 
-            <nav>
-                <a href="#">
-                    < i class='bxr  bx-user-circle'  ></i> Home
+            <nav className="hidden md:flex lg:gap-8 gap-4">
+                <a href="#" className="flex relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:bottom-0 after:left-0 after:transition-all hover:after:w-full text-nowrap items-center gap-2">
+                    <FaRegUserCircle /> Avatar
+                </a>
+                <a href="#" className="flex relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:bottom-0 after:left-0 after:transition-all hover:after:w-full text-nowrap items-center gap-2">
+                    <IoDiamondOutline /> Arena
+                </a>
+                <a href="#" className="flex relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:bottom-0 after:left-0 after:transition-all hover:after:w-full text-nowrap items-center gap-2">
+                    <MdKeyboardDoubleArrowUp /> Beyond
+                </a>
+                <a href="#" className="flex relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:bottom-0 after:left-0 after:transition-all hover:after:w-full text-nowrap items-center gap-2">
+                    <BsShop />Shop
                 </a>
             </nav>
+
+            <button onClick={handleMenu} className="text-3xl p-2 md:hidden">
+                {menu ? <IoMdClose /> : <IoMdMenu />  }
+            </button>
+
+            {menu && (
+                <div className="fixed top-14 right-0 left-0 bg-black p-5 md:hidden">
+                    <nav className="flex flex-col gap-4 items-center">
+                        <a href="#" className="flex relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:bottom-0 after:left-0 after:transition-all hover:after:w-full text-nowrap items-center gap-2">
+                            <FaRegUserCircle /> Avatar
+                        </a>
+                        <a href="#" className="flex relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:bottom-0 after:left-0 after:transition-all hover:after:w-full text-nowrap items-center gap-2">
+                            <IoDiamondOutline /> Arena
+                        </a>
+                        <a href="#" className="flex relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:bottom-0 after:left-0 after:transition-all hover:after:w-full text-nowrap items-center gap-2">
+                            <MdKeyboardDoubleArrowUp /> Beyond
+                        </a>
+                        <a href="#" className="flex relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:bottom-0 after:left-0 after:transition-all hover:after:w-full text-nowrap items-center gap-2">
+                            <BsShop />Shop
+                        </a>
+                    </nav>
+
+                    <div className="flex flex-col space-y-2 mt-5">
+                        <button className="bg-purple-700 py-2 rounded w-full">
+                            Play Now
+                        </button>
+
+                        <button className="bg-gray-500 py-2 rounded w-full">
+                            NFT Store
+                        </button>
+                    </div>
+                </div>
+            )}
         </header>
     )
 }
