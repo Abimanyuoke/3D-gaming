dc25fbab1f8ab3e8c97356039caeb13560cf0ac9
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-06-08 20:52:29 +0700

    `Moved all public assets to src/assets/public directory`

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ import { FaFacebookSquare } from "react-icons/fa";
 export default function Footer() {
     return (
         <footer className="flex items-center justify-between lg:mt-[15%] mt-[25%] py-8 lg:px-32 md:px-16 px-8 border-t-[0.3px] border-[#babaff]">
-            <img src="src/public/images/illu-text.png" alt="Illu-Text" className="h-10" />
-            <img src="src/public/images/illu-logo.png" alt="Illu-Logo" className="h-16 md:inline hidden" />
+            <img src="src/assets/public/images/illu-text.png" alt="Illu-Text" className="h-10" />
+            <img src="src/assets/public/images/illu-logo.png" alt="Illu-Logo" className="h-16 md:inline hidden" />
             <div className="flex gap-4">
                 <div className="flex items-center cursor-pointer">
                     <FaLinkedin/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
     return (
         <header className="py-1 px-7 flex justify-between items-center top-0 z-50 w-full border-b-[0.3px] border-[#babaff] bg-black sticky">
             <div className="flex lg:gap-14 gap-4 items-center">
-                <img src="src/public/images/logo.png" alt="logo-img" className="md:w-16 w-12" />
+                <img src="src/assets/public/images/logo.png" alt="logo-img" className="md:w-16 w-12" />
                 <div className="hidden md:flex gap-5">
                     <button className="bg-gradient-to-r from-purple-500 to-indigo-600 rounded-lg h-8 px-6 font-medium text-nowrap hover:opacity-70 transition-all duration-300">
                         PLAY NOW
