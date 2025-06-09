7555bbc81bcec9b7735653162bdc3a0630b07ae7
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-06-09 09:02:25 +0700

    fix: update image and video imports to use variables for better path management

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,14 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaDiscord } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
+import illutext from "src/assets/images/illu-text.png"
+import illulogo from "src/assets/images/illu-logo.png"
 
 export default function Footer() {
     return (
         <footer className="flex items-center justify-between lg:mt-[15%] mt-[25%] py-8 lg:px-32 md:px-16 px-8 border-t-[0.3px] border-[#babaff]">
-            <img src="src/assets/images/illu-text.png" alt="Illu-Text" className="h-10" />
-            <img src="src/assets/images/illu-logo.png" alt="Illu-Logo" className="h-16 md:inline hidden" />
+            <img src={illutext} alt="Illu-Text" className="h-10" />
+            <img src={illulogo} alt="Illu-Logo" className="h-16 md:inline hidden" />
             <div className="flex gap-4">
                 <div className="flex items-center cursor-pointer">
                     <FaLinkedin/>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
+import hero from "src/assets/videos/hero.mp4"
+import illutext from "src/assets/images/illu-text.png"
+import illulogo from "src/assets/images/illu-logo.png"
+
 export default function Hero() {
     return (
         <main className="relative w-full h-screen overflow-hidden flex justify-center mb-[10%]">
-            <video src="src/assets/videos/hero.mp4" autoPlay loop muted playsInline className="w-full h-[95%] object-cover absolute top-0 first-letter:left-0 -z-10"></video>
+            <video src={hero} autoPlay loop muted playsInline className="w-full h-[95%] object-cover absolute top-0 first-letter:left-0 -z-10"></video>
             <div className="absolute bottom-[15%] flex flex-col items-center gap-5">
-                <img src="src/assets/images/illu-text.png" alt="illu-text" className="md:w-[30rem] w-[20rem]" />
+                <img src={illutext} alt="illu-text" className="md:w-[30rem] w-[20rem]" />
                 <h1 className="md:text-2xl text-1xl font-bold">Explore, Capture, Conquer</h1>
 
                 <div className="md:w-[75%] w-[60%] h-[0.1px] bg-[#baba]"></div>
@@ -13,7 +17,7 @@ export default function Hero() {
                 </button>
 
                 <div className="flex items-center gap-5 text-3xl font-extrabold text-gray-200 ">
-                    <img src="src/assets/images/illu-logo.png" alt="illu-logo" className="md:h-16 h-12" /> ZERO
+                    <img src={illulogo} alt="illu-logo" className="md:h-16 h-12" /> ZERO
                 </div>
 
                 <p className="max-w-[80%] text-center text-[#babaff]">Notice: Illuvium Games are in Beta. Participation involves risk. Read our full Disclaimer here.</p>
